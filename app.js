14f9021b725e1a36ea4cc4ce3c95b631c1442d4e
jorgehg <j.herrando.dev@gmail.com>
2021-08-09 12:25:56 -0300

    CRUD de productos + JSON DB + pequeños ajustes visuales CSS

--- app.js
+++ app.js
@@ -1,28 +1,44 @@
+// ************ Require's ************
 const express = require("express");
-const app = express();
 const path = require("path");
 const methodOverride = require("method-override");
+// Para poder usar los métodos PUT y DELETE
+
+// ************ express() ************
+const app = express();
+
+// ************ Middlewares ************
+
+app.use(express.static("public")); // Necesario para los archivos estáticos en el folder /public
+app.use(express.urlencoded({ extended: false })); // to recognize the incoming Request Object as strings or arrays
+app.use(express.json()); // to recognize the incoming Request Object as a JSON Object
+app.use(methodOverride("_method")); // Pasar poder pisar el method="POST" en el formulario por PUT y DELETE
+
+// ************ Template Engine ************
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "src/views")); // express accede directamente a views a la hora de renderizar si está en la raíz del proyecto. Si no (está dentro de 'src'), hay que darle la ruta con el siguiente comando
+
+// ************ Route System require and use() ************
 
 const mainRouter = require("./src/routes/main");
 const usersRouter = require("./src/routes/users");
 const productsRouter = require("./src/routes/products");
 
-app.use(express.static("public"));
-
-// express accede directamente a views a la hora de renderizar si está en la raíz del proyecto. Si no (está dentro de 'src'), hay que darle la ruta con el siguiente comando
-app.set("views", path.join(__dirname, "src/views"));
-app.set("view engine", "ejs");
-
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
-app.use(methodOverride("_method"));
-
 app.use("/", mainRouter);
 app.use("/users", usersRouter);
 app.use("/products", productsRouter);
+
+// ************ catch 404 and forward to error handler ************
+app.use((req, res, next) => {
+  res.status(404).render("./main/404", { title: "Havenboards - Not found" });
+});
 
 let mensaje = () => {
   console.log("Servidor funcionando en puerto 3000");
 };
 
 app.listen(3000, mensaje());
+
+// ************ exports app ************
+// module.exports = app;
--- src/controllers/productsController.js
+++ src/controllers/productsController.js
@@ -1,41 +1,17 @@
-// const products = [{}];
-const datosPrecio = [
-  "Precio 1",
-  "Precio 2",
-  "Precio 3",
-  "Precio 4",
-  "Precio 5",
-  "Precio 6",
-  "Precio 7",
-  "Precio 8",
-  "Precio 9",
-  "Precio 10",
-  "Precio 11",
-  "Precio 12",
-];
-const datosTitulo = [
-  "Título 1",
-  "Título 2",
-  "Título 3",
-  "Título 4",
-  "Título 5",
-  "Título 6",
-  "Título 7",
-  "Título 8",
-  "Título 9",
-  "Título 10",
-  "Título 11",
-  "Título 12",
-];
+const fs = require("fs");
+const path = require("path");
+
+const productsFilePath = path.join(__dirname, "../data/products.json"); //ruta a nuestra DB JSON
+let products = JSON.parse(fs.readFileSync(productsFilePath, "utf-8")); // pasamos de formato JSON a JS
+
+// esto era de prueba
 const categoria = ["Snowboard"];
 
 const productsController = {
   shop: (req, res) => {
     res.render("products/shop", {
       title: "Havenboards - Tienda",
-      precio: datosPrecio,
-      titulo: datosTitulo,
-      categoria: categoria,
+      products: products, //le pasamos todos los productos con todas sus key:value
     });
   },
   productCart: (req, res) => {
@@ -43,28 +19,102 @@ const productsController = {
       title: "Havenboards - Shopping Cart",
     });
   },
-  productDetail: (req, res) => {
+  detail: (req, res) => {
+    let id = req.params.id; //recuperamos el param del url
+
+    let detailProduct = products.find((elem) => elem.id == id); //encontramos el elem con el mismo id en nuestra DB ya transformada
+
     res.render("products/productDetail", {
       title: "Havenboards - Product Detail",
+      detailProduct: detailProduct, //mandamos el producto que deseamos
     });
   },
-  productUpView: (req, res) => {
-    res.render("products/productUp", {
+  create: (req, res) => {
+    res.render("products/createProduct", {
       title: "Havenboards - Loading Product",
     });
   },
-  productUpUpload: (req, res) => {
-    let dataForm = req.body;
-    res.redirect("/products/productUp");
+  store: (req, res) => {
+    // con req.files accedemos a todos los file mandados y guardados en array. Solo queremos el nombre así que creamos nuevo array donde los pushearemos
+    let images = [];
+    for (i = 0; i < req.files.length; i++) {
+      images.push(req.files[i].originalname);
+    }
+
+    // crear nuevo id para producto creado
+    let newId = products[products.length - 1].id + 1;
+
+    // le damos los valores al nuevo producto de cada uno de los campos del form
+    let newProduct = {
+      name: req.body.name,
+      id: newId,
+      description: req.body.description,
+      // le pasamos el array con los nombres
+      image: images,
+      category: req.body.category,
+      subcategory: req.body.subcategory,
+      brand: req.body.brand,
+      price: req.body.price,
+      size: req.body.size,
+      discount: req.body.discount,
+      sale: req.body.sale,
+    };
+
+    // agregamos nuevo producto
+    products.push(newProduct);
+
+    // reescribimos la BD en formato JSON
+    fs.writeFileSync(productsFilePath, JSON.stringify(products));
+
+    res.redirect("/products/detail/" + newId);
   },
+
   productEditView: (req, res) => {
+    let id = req.params.id;
+
+    let editProduct = products.find((elem) => elem.id == id);
+
     res.render("products/productEdit", {
       title: "Havenboards - Editing Product",
+      editProduct: editProduct,
     });
   },
   productEditUpload: (req, res) => {
-    let dataForm = req.body;
-    res.redirect("/products/productEdit");
+    let id = req.params.id;
+
+    let editedProduct = products.find((elem) => elem.id == id);
+    // con req.files accedemos a todos los file mandados y guardados en array. Solo queremos el nombre así que creamos nuevo array donde los pushearemos
+    let images = [];
+    for (i = 0; i < req.files.length; i++) {
+      images.push(req.files[i].originalname);
+    }
+
+    // cambiamos los values de las key
+    editedProduct.name = req.body.name;
+    editedProduct.description = req.body.description;
+    editedProduct.image = images; // le pasamos el array con los nombres de img
+    editedProduct.category = req.body.category;
+    editedProduct.subcategory = req.body.subcategory;
+    editedProduct.brand = req.body.brand;
+    editedProduct.price = req.body.price;
+    editedProduct.size = req.body.size;
+    editedProduct.discount = req.body.discount;
+    editedProduct.sale = req.body.sale;
+
+    fs.writeFileSync(productsFilePath, JSON.stringify(products));
+
+    res.redirect("/products/detail/" + id);
+  },
+
+  destroy: (req, res) => {
+    let id = req.params.id;
+
+    // products pasa a ser un array de todos los productos excepto del que queremos eliminar
+    products = products.filter((elem) => elem.id != id);
+
+    let productsJSON = JSON.stringify(products); // lo pasamos a JSON
+    fs.writeFileSync(productsFilePath, productsJSON); //escribimos la nueva DB
+    res.redirect("/"); //redireccionamos al home
   },
 };
 module.exports = productsController;
